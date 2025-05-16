3b77b638d8c298e66df68f4d80126f69ba1ad457
nicejmp1 <a01063363854@gmail.com>
2025-05-16 22:25:05 +0900

    feat: 아이디, 패스워드 Page 구현

---
+++ src/app/auth/find/id/page.tsx
@@ -0,0 +1,11 @@
+import { getPageMetadata } from '@/lib/metadata';
+
+export default function FindIdPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+                <h1 className="text-2xl font-bold mb-6">아이디 찾기</h1>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/auth/find/password/page.tsx
@@ -0,0 +1,13 @@
+import FindIdForm from "@/components/auth/find/password/FindPwForm";
+import { getPageMetadata } from "@/lib/metadata";
+
+export default function FindPwPage() {
+    return(
+        <div className="min-h-screen flex items-center justify-center">
+            <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-lg">
+                <h1 className="text-2xl font-bold mb-6">비밀번호 찾기</h1>
+                <FindIdForm />
+            </div>
+        </div>
+    )
+}
